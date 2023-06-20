2f9393eb240e72266a1b1805e533d34295435eee
Aleksandr Demenin <demenin.aleksandr@yandex.ru>
2023-06-20 08:50:47 +0700

    модуль с настройками экрана

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,6 +1,6 @@
 class Settings():
     def __init__(self):
-        '''Инициализирует настройки игры'''
+        '''Инициализирует настройки игры1'''
         #Параметры экрана
         self.screen_width  = 1200
         self.screen_heigth = 800
